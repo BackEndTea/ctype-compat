c2b1bb7e9d928cf555d3d6dcc5fe3583c09c4001
Gert de Pagter <gertp93@gmail.com>
2018-04-23 22:19:44 +0200

    Add a composer test
    By requiring the vendor/autoload.php an error will be generated if there
    is an error with our file, or if there is a typo in the autoload files
    part of the composer.json.
    
    This gives us a bit more certainty we did not mess up anything.

--- tests/tests.php
+++ tests/tests.php
@@ -23,6 +23,8 @@ include_once __DIR__.'/TestRunner.php';
 $testRunner = new TestRunner();
 if(TestRunner::run()){
     echo "All tests ran succsessfully.\n";
+    //If our real file produces an error or is not present this will error.
+    require_once __DIR__ . '/../vendor/autoload.php';
     exit(0);
 }
 
